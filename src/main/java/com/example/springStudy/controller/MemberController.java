8f8955ad170354bd71a4f3f91fe8ecbc31a41179
yh <ks00141@naver.com>
2022-04-13 21:51:46 +0900

    회원 등록 컨트롤러

--- src/main/java/com/example/springStudy/controller/MemberController.java
+++ src/main/java/com/example/springStudy/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.example.springStudy.controller;
 
+import com.example.springStudy.domain.Member;
 import com.example.springStudy.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMembersForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String List(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "/members/memberList";
     }
 }
